94e5c98cbb67da422f23e1b5348100660d09ebee
Andrea Schioppa <andrea.schioppa@booking.com>
2019-01-22 14:51:38 +0100

    Adding License Information

--- R/AutoPasteFromScript.R
+++ R/AutoPasteFromScript.R
@@ -1,7 +1,23 @@
+#
+#  Copyright 2019 Andrea Schioppa
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ######################################################
 # interactiveScript is a reference class
 # to interactively execute code from fileName
-# 
+#
 # instantiate interactiveScript from a file name:
 # myS = AutoPasteFromScript$new(fileName=" ")
 #
@@ -18,7 +34,7 @@ AutoPasteFromScript <- setRefClass("AutoPasteFromScript",
          lines <<- scan(fileName, what = character(), quiet=TRUE, sep = "\n",
                         blank.lines.skip = FALSE)
      },
-     
+
      toString = function(min, max) {
          load()
 
@@ -41,4 +57,4 @@ AutoPasteFromScript <- setRefClass("AutoPasteFromScript",
          }
  )
  )
-                                     
+
